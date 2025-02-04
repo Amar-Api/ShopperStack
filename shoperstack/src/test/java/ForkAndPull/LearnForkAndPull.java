fe85425976f05285e2716b5a606b3d26c20bf1c9
Amrendra282 <amrendramishra54@gmail.com>
2025-02-04 11:31:17 +0530

    commit fork

---
+++ shoperstack/src/test/java/ForkAndPull/LearnForkAndPull.java
@@ -0,0 +1,11 @@
+package ForkAndPull;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LearnForkAndPull {
+     @Test
+     public void login() {
+    	 Reporter.log("Fork and pull", true);
+     }
+}
